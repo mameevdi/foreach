e51fe3c091868a24edf4a8b1f35c2f636902bf88
mameevdi <mameevs@gmail.com>
2024-02-29 03:03:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40};
+
+        for (int i: array) {
+            System.out.print(i + " ");
+        }
+    }
+}
