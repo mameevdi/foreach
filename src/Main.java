ff1cdad7fc3bed73bafafa1c4b06e8e6056f99ae
mameevdi <mameevs@gmail.com>
2024-03-03 11:46:50 +0300

    Сделал вывод в консоль приветствия!

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int[] array = {10, 20, 30, 40};
-
-        for (int i: array) {
-            System.out.print(i + " ");
-        }
+        System.out.println("Привет!");
     }
 }
